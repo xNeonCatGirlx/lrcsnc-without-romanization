f1acbb58a19ce8b2934b6d02ee2a675d0c9046d1
Endg4me_ <timka.tarasen@gmail.com>
2025-03-09 20:12:26 +1000

    refactor(log): reorganize and refactor the code

--- internal/log/log.go
+++ internal/log/log.go
@@ -2,11 +2,13 @@ package log
 
 import (
 	"fmt"
-	"lrcsnc/internal/config"
 	"os"
 	"path"
 	"strings"
 	"time"
+
+	"lrcsnc/internal/pkg/global"
+	"lrcsnc/internal/pkg/types"
 
 	"github.com/aerogo/log"
 )
@@ -14,86 +16,90 @@ import (
 var logger *log.Log
 
 func Init() {
-	if !config.CurrentConfig.Config.Global.Log.Enabled {
+	if !global.Config.C.Global.Log.Enabled {
 		return
 	}
 
 	logger = log.New()
 
 	// Check for the file to be writeable, otherwise panic
-	if err := os.MkdirAll(path.Dir(os.ExpandEnv(config.CurrentConfig.Config.Global.Log.Destination)), 0777); err != nil {
-		panic(fmt.Errorf("[log/Init] FATAL: Log directory could not be created. Either disable logs before launch, or change the directory and try again.\n\nMore:\n%s", err.Error()))
+	if err := os.MkdirAll(path.Dir(os.ExpandEnv(global.Config.C.Global.Log.Destination)), 0777); err != nil {
+		Fatal("log/Init", fmt.Sprintf("Log directory could not be created. Either disable logs before launch, or change the directory and try again.\n\nMore:\n%s", err.Error()))
 	}
-	f, err := os.Create(os.ExpandEnv(config.CurrentConfig.Config.Global.Log.Destination))
+	f, err := os.Create(os.ExpandEnv(global.Config.C.Global.Log.Destination))
 	if err != nil {
-		panic(fmt.Errorf("[log/Init] FATAL: Log file could not be created. Either disable logs before launch, or change the file path and try again.\n\nMore:\n%s", err.Error()))
+		Fatal("log/Init", fmt.Sprintf("Log file could not be created. Either disable logs before launch, or change the file path and try again.\n\nMore:\n%s", err.Error()))
 	}
-	f.Close()
+	err = f.Close()
+	if err != nil {
+		Fatal("log/Init", fmt.Sprintf("Log file could not be closed after creation. Either disable logs before launch, or change the file path and try again.\n\nMore:\n%s", err.Error()))
+	}
 
-	logger.AddWriter(log.File(os.ExpandEnv(config.CurrentConfig.Config.Global.Log.Destination)))
+	logger.AddWriter(log.File(os.ExpandEnv(global.Config.C.Global.Log.Destination)))
 }
 
-// A log function specifically for detailed debug information.
+// Debug is a log function specifically for detailed debug information.
 func Debug(modulePath string, message string) {
 	// Every log should be asynchronous
 	go func() {
-		config.CurrentConfig.Mutex.Lock()
-		defer config.CurrentConfig.Mutex.Unlock()
-		if config.CurrentConfig.Config.Global.Log.Level.ToInt() < 4 {
+		global.Config.M.Lock()
+		defer global.Config.M.Unlock()
+		if !global.Config.C.Global.Log.Enabled || global.Config.C.Global.Log.Level.ToInt() < 4 {
+
 			return
 		}
 		t := time.Now().Format(time.DateTime)
-		logger.Info(t + " " + modulePath + " " + strings.ToUpper(string(config.LogLevelDebug)) + ": " + message)
+		logger.Info(t + " [" + modulePath + "] " + strings.ToUpper(string(types.LogLevelDebug)) + ": " + message)
 	}()
 }
 
-// A general-use log function for stuff like start/stop, config updates, etc.
+// Info is a general-use log function for stuff like start/stop, config updates, etc.
 func Info(modulePath string, message string) {
 	// Every log should be asynchronous
 	go func() {
-		config.CurrentConfig.Mutex.Lock()
-		defer config.CurrentConfig.Mutex.Unlock()
-		if config.CurrentConfig.Config.Global.Log.Level.ToInt() < 3 {
+		global.Config.M.Lock()
+		defer global.Config.M.Unlock()
+		if !global.Config.C.Global.Log.Enabled || global.Config.C.Global.Log.Level.ToInt() < 3 {
 			return
 		}
 		t := time.Now().Format(time.DateTime)
-		logger.Info(t + " " + modulePath + " " + strings.ToUpper(string(config.LogLevelInfo)) + ": " + message)
+		logger.Info(t + " [" + modulePath + "] " + strings.ToUpper(string(types.LogLevelInfo)) + ": " + message)
 	}()
 }
 
-// A log function specifically for warnings.
+// Warn is a log function specifically for warnings.
 func Warn(modulePath string, message string) {
 	// Every log should be asynchronous
 	go func() {
-		config.CurrentConfig.Mutex.Lock()
-		defer config.CurrentConfig.Mutex.Unlock()
-		if config.CurrentConfig.Config.Global.Log.Level.ToInt() < 2 {
+		global.Config.M.Lock()
+		defer global.Config.M.Unlock()
+		if !global.Config.C.Global.Log.Enabled || global.Config.C.Global.Log.Level.ToInt() < 2 {
 			return
 		}
 		t := time.Now().Format(time.DateTime)
-		logger.Info(t + " " + modulePath + " " + strings.ToUpper(string(config.LogLevelWarn)) + ": " + message)
+		logger.Info(t + " [" + modulePath + "] " + strings.ToUpper(string(types.LogLevelWarn)) + ": " + message)
 	}()
 }
 
-// A log function specifically for non-fatal errors.
+// Error is a log function specifically for non-fatal errors.
 func Error(modulePath string, message string) {
 	// Every log should be asynchronous
 	go func() {
-		config.CurrentConfig.Mutex.Lock()
-		defer config.CurrentConfig.Mutex.Unlock()
-		if config.CurrentConfig.Config.Global.Log.Level.ToInt() < 1 {
+		global.Config.M.Lock()
+		defer global.Config.M.Unlock()
+		if !global.Config.C.Global.Log.Enabled || global.Config.C.Global.Log.Level.ToInt() < 1 {
 			return
 		}
 		t := time.Now().Format(time.DateTime)
-		logger.Error(t + " " + modulePath + " " + strings.ToUpper(string(config.LogLevelError)) + ": " + message)
+		logger.Error(t + " [" + modulePath + "] " + strings.ToUpper(string(types.LogLevelError)) + ": " + message)
 	}()
 }
 
-// A log function specifically for fatal errors. 
+// Fatal is a log function specifically for fatal errors.
 // Immediately exits the app.
 // Doesn't write to file: instead uses stdout to ensure the message gets to user.
 func Fatal(modulePath string, message string) {
 	t := time.Now().Format(time.DateTime)
-	fmt.Println(t + " " + modulePath + " " + strings.ToUpper(string(config.LogLevelFatal)) + ": " + message)
+	fmt.Println(t + " [" + modulePath + "] " + strings.ToUpper(string(types.LogLevelFatal)) + ": " + message)
 	os.Exit(1)
 }
