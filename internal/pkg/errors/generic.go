07649d762708939d12007c1638c00ae17ecaf9df
Endg4me_ <65147948+Endg4meZer0@users.noreply.github.com>
2025-05-02 07:20:35 +1000

    feat(pkg): add generic errors

--- internal/pkg/errors/generic.go
+++ internal/pkg/errors/generic.go
@@ -11,11 +11,17 @@ var ErrUnmarshalFail = errors.New("encountered unmarshal error")
 // ErrFileUnreachable represents an error indicating that a file could not be reached or accessed.
 var ErrFileUnreachable = errors.New("file is unreachable")
 
+// ErrFileUnreadable represents an error indicating that a file could not be read.
+var ErrFileUnreadable = errors.New("file is unreadable")
+
 // ErrFileUnwriteable represents an error indicating that a file could not be created or written in.
 var ErrFileUnwriteable = errors.New("file is unwriteable")
 
 // ErrDirUnreachable represents an error indicating that a directory could not be reached or accessed.
 var ErrDirUnreachable = errors.New("directory is unreachable")
 
+// ErrDirUnreadable represents an error indicating that a directory could not be read.
+var ErrDirUnreadable = errors.New("directory is unreadable")
+
 // ErrDirUnwriteable represents an error indicating that a directory could not be created or written in.
-var ErrDirUnwriteable = errors.New("file is unwriteable")
+var ErrDirUnwriteable = errors.New("directory is unwriteable")
